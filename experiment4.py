c82b04223c2171f1d220d2e075ef2773c4787d90
piyushg9571 <143926481+piyushg9571@users.noreply.github.com>
2024-09-29 15:53:04 +0530

    Update experiment4.py

--- experiment4.py
+++ experiment4.py
@@ -6,23 +6,23 @@
 import pandas as pd
 
 # Import data from a CSV file
-data = pd.read_csv('student-scores.csv')
+df = pd.read_csv('student-scores.csv')
 
 # Display dataset details
-print("Number of rows:", data.shape[0])
-print("Number of columns:", data.shape[1])
-print("\nFirst five rows:\n", data.head())
-print("\nSize of the dataset:", data.size)
+print("Number of rows:", df.shape[0])
+print("Number of columns:", df.shape[1])
+print("\nFirst five rows:\n", df.head())
+print("\nSize of the dataset:", df.size)
 
 # Cheking if there is any missing values in Data set
-print("\nNumber of missing values:\n", data.isnull().sum())
+print("\nNumber of missing values:\n", df.isnull().sum())
 
 # Display summary statistics for numerical columns
 # summision of all numerical values present in Dataset
-print("\nSum of numerical columns:\n", data.sum(numeric_only=True))
-print("\nAverage of numerical columns:\n", data.mean(numeric_only=True))
-print("\nMinimum values of numerical columns:\n", data.min(numeric_only=True))
-print("\nMaximum values of numerical columns:\n", data.max(numeric_only=True))
+print("\nSum of numerical columns:\n", df.sum(numeric_only=True))
+print("\nAverage of numerical columns:\n", df.mean(numeric_only=True))
+print("\nMinimum values of numerical columns:\n", df.min(numeric_only=True))
+print("\nMaximum values of numerical columns:\n", df.max(numeric_only=True))
 
 # Export data to a new CSV file
-data.to_csv('output.csv', index=False)
+df.to_csv('output.csv', index=False)
